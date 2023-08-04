d008afa00a0d8aea867c55e75497a2aa69480885
Mark <ajineer@outlook.com>
2023-08-04 10:56:11 -0600

    completed lab I guess

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,28 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key = True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    reviews = relationship('Review', backref=backref('game'))
+
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + '\n' f'title={self.title}, ' + '\n' f'platform={self.platform})' 
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(String(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(id={self.id}, '  + '\n' f'score = {self.score}, ' + '\n' f'game_id = {self.game_id})'
